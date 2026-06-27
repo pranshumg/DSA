b1ca2cc068b1aa811a3579eed4179bcbde8829b9
Pranshu Gupta <pranshumg@gmail.com>
2026-06-27 14:14:46 +0530

    initial commit

---
+++ strings/remove_outermost_parentheses.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Remove outermost parentheses */
+// https://leetcode.com/problems/remove-outermost-parentheses/
+
+// TC - O(n), SC - O(n)
+string remove_outermost_parentheses(string s) {
+    string res = "";
+    int cnt = 0;
+    for (char c : s) {
+        if (c == '(') {
+            if (cnt > 0) res += c;
+            cnt++;
+        } else {
+            cnt--;
+            if (cnt > 0) res += c;
+        }
+    }
+    return res;
+}
