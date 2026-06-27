234b3bd54ea4a88c02e55f13b66c83d65d203921
Pranshu Gupta <pranshumg@gmail.com>
2026-06-27 14:26:49 +0530

    initial commit

---
+++ strings/reverse_words.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Reverse words in a string */
+// https://leetcode.com/problems/reverse-words-in-a-string/
+
+// TC - O(n), SC - O(n)
+string reverse_words(string s) {
+    string res = "";
+    for (int i = int(s.size()) - 1; i >= 0; i--) {
+        string word = "";
+        while (i >= 0 && s[i] != ' ') {
+            word = s[i--] + word;
+        }
+        if (!word.empty()) {
+            res += " " + word;
+        }
+    }
+    return res.empty() ? "" : res.substr(1);
+}
