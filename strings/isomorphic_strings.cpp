f1739c726d5f1836bf7ff9216b49038f2643c23e
Pranshu Gupta <pranshumg@gmail.com>
2026-06-27 15:03:47 +0530

    initial commit

---
+++ strings/isomorphic_strings.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Isomorphic Strings */
+// https://leetcode.com/problems/isomorphic-strings/
+
+// TC - O(n), SC - O(1)
+bool is_isomorphic(string s, string t) {
+    if (int(s.size()) != int(t.size())) {
+        return false;
+    }
+    map<char, char> mp_s, mp_t;
+    for (int i = 0; i < int(s.size()); i++) {
+        if (mp_s.count(s[i]) && mp_s[s[i]] != t[i]) return false;
+        if (mp_t.count(t[i])  && mp_t[t[i]] != s[i]) return false;
+        mp_s[s[i]] = t[i];
+        mp_t[t[i]] = s[i];
+    }
+    return true;
+}
