cd729170fcd243ea12ba97753029b73bf9abb873
Pranshu Gupta <pranshumg@gmail.com>
2026-06-27 16:33:30 +0530

    initial commit

---
+++ strings/max_nesting_depth_of_parentheses.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Maximum nesting depth of parentheses */
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+// TC - O(n), SC - O(1)
+int max_depth(string s) {
+    int ans = 0, cnt = 0;
+    for (char c : s) {
+        if (c == '(') cnt++;
+        else if(c == ')') cnt--;
+        ans = max(ans, cnt);
+    }
+    return ans;
+}
