60a162b44ebc944de9ff2c8464dfa770a10192bf
Pranshu Gupta <pranshumg@gmail.com>
2026-06-29 22:47:30 +0530

    initial commit

---
+++ linked_list/search.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Search in linked list */
+
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->next = nullptr;
+    }
+};
+
+// TC - O(n), SC - O(1)
+bool search(Node* head, int target) {
+    for (Node* tmp = head; tmp; tmp = tmp->next) {
+        if (tmp->info == target) {
+            return true;
+        }
+    }
+    return false;
+}
