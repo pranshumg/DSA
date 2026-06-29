2cebd8362e217e5a1bdac2f7a8216c1c9f728de9
Pranshu Gupta <pranshumg@gmail.com>
2026-06-29 22:43:39 +0530

    initial commit

---
+++ linked_list/length.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Length of linked list */
+
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->next = nullptr;
+    }
+};
+
+// TC - O(n), SC - O(1)
+int length(Node* head) {
+    int cnt = 0;
+    for (Node* tmp = head; tmp; tmp = tmp->next) {
+        cnt++;
+    }
+    return cnt;
+} 
