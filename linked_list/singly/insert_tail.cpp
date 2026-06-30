2726c23aff28d5e608d3f7ba385026d76c08971d
Pranshu Gupta <pranshumg@gmail.com>
2026-06-30 16:28:04 +0530

    initial commit

---
+++ linked_list/singly/insert_tail.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Insert tail */
+
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->next = nullptr;
+    }
+
+    Node(int info, Node* next) {
+        this->info = info;
+        this->next = next;
+    }
+};
+
+// TC - O(n), SC - O(1)
+Node* insert_tail(Node* head, int val) {
+    if (!head) {
+        return new Node(val);
+    }
+    Node* cur = head;
+    while (cur->next) {
+        cur = cur->next;
+    }
+    cur->next = new Node(val);
+    return head;
+}
