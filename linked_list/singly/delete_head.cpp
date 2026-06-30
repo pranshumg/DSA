dc9d578e9684597850f27ef3e51b7c4db042bb04
Pranshu Gupta <pranshumg@gmail.com>
2026-06-30 14:28:32 +0530

    initial commit

---
+++ linked_list/singly/delete_head.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete head */
+
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->next = nullptr;
+    }
+};
+
+// TC - O(1), SC - O(1)
+Node* delete_head(Node* head) {
+    if (!head) {
+        return nullptr;
+    }
+    Node* tmp = head;
+    head = head->next;
+    delete tmp;
+    return head;
+}
