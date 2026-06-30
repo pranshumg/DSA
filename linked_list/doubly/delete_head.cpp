fdb5f2411290521f78ecd0539a7df4744e52715e
Pranshu Gupta <pranshumg@gmail.com>
2026-06-30 19:38:37 +0530

    initial commit

---
+++ linked_list/doubly/delete_head.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete head */
+
+class Node {
+public:
+    int info;
+    Node* prev;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->prev = nullptr;  
+        this->next = nullptr;
+    }
+};
+
+// TC - O(1), SC - O(1)
+Node* delete_head(Node* head) {
+    if (!head) {
+        return head;
+    }
+    if (!head->next) {
+        delete head;
+        return nullptr;
+    }
+    Node* tmp = head;
+    head = head->next;
+    head->prev = nullptr;
+    delete tmp;
+    return head;
+}
