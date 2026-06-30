55d1598922394e4a64533ab5adfd687bd9d89730
Pranshu Gupta <pranshumg@gmail.com>
2026-06-30 19:42:34 +0530

    initial commit

---
+++ linked_list/doubly/delete_tail.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete tail */
+
+class Node {
+public:
+    int info;
+    Node* prev;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->prev = nullptr;  
+        this->next = nullptr;
+    }
+};
+
+// TC - O(n), SC - O(1)
+Node* delete_tail(Node* head) {
+    if (!head) {
+        return head;
+    }
+    if (!head->next) {
+        delete head;
+        return nullptr;
+    }
+    Node* tmp = head;
+    while (tmp->next->next) {
+        tmp = tmp->next;
+    }
+    delete tmp->next;
+    tmp->next = nullptr;
+    return head;
+}
