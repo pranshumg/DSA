00d922ffa9bab99d9f7d433cf97485d93b903cb0
Pranshu Gupta <pranshumg@gmail.com>
2026-06-30 14:36:44 +0530

    initial commit

---
+++ linked_list/singly/delete_tail.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete tail */
+
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->next = nullptr;
+    }
+};
+
+// TC - O(n), SC - O(1)
+Node* delete_tail(Node* head) {
+    if (!head) {
+        return nullptr;
+    }
+    if (!head->next) {
+        delete head;
+        return nullptr;
+    }
+    Node* tmp = head;
+    while (tmp->next->next) {
+        tmp = tmp->next;
+    }
+    delete tmp->next;
+    tmp->next = nullptr;
+    return head;
+}
