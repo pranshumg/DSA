8d6f964596378e1639e8ad36947d55f7cb1eb640
Pranshu Gupta <pranshumg@gmail.com>
2026-07-01 15:02:27 +0530

    initial commit

---
+++ linked_list/doubly/insert_node.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Insert node before target */
+
+class Node {
+public:
+    int info;
+    Node* prev;
+    Node* next;
+
+    Node(int info) {
+        this->info = info;
+        this->prev = nullptr;  
+        this->next = nullptr;
+    }
+
+    Node(int info, Node* prev, Node* next) {
+        this->info = info;
+        this->prev = prev;  
+        this->next = next;
+    }
+};
+
+// TC - O(n), SC - O(1)
+Node* insert_node(Node* head, int val, int target) {
+    if (!head) {
+        return head;
+    }
+    if (head->info == target) {
+        Node* node = new Node(val, nullptr, head);
+        head->prev = node;
+        return node;
+    }
+    Node* cur = head;
+    while (cur->next && cur->next->info != target) {
+        cur = cur->next;
+    }
+    if (!cur->next) {
+        return head;
+    }
+    Node* node = new Node(val, cur, cur->next);
+    cur->next->prev = node;
+    cur->next = node;
+    return head;
+}
