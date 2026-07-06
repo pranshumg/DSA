51b0bf95ed4a7e7ea55317b0d5c971a8319b6a45
Pranshu Gupta <pranshumg@gmail.com>
2026-07-07 03:26:31 +0530

    initial commit

---
+++ linked_list/middle.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Middle of a linked list */
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int info) {
+        this->info = info; 
+        this->next = nullptr;
+    }
+
+    Node(int info, Node* next) {
+        this->info = info;  
+        this->next = next;
+    }
+};
+
+// TC - O(n), SC - O(1)
+Node* middle(Node* head) {
+    int n = 0;
+    Node* cur = head;
+    while (cur) {
+        n++;
+        cur = cur->next;
+    }
+    int mid = n / 2;
+    cur = head;
+    while (mid--) {
+        cur = cur->next;
+    }
+    return cur;
+}
+
+// TC - O(n), SC - O(1)
+Node* middle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+    while (fast && fast->next) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
