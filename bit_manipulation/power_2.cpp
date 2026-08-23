061925972c8db06abcda3fe3543b33a9cb29bf1a
pranshumg <pranshumg@gmail.com>
2026-08-23 18:49:37 +0530

    intial commit

---
+++ bit_manipulation/power_2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* power of 2 */
+// https://leetcode.com/problems/power-of-two/
+
+bool is_power_of_2(int n) {
+  if (n == 0 || n == INT_MIN || n == INT_MAX) {
+    return false;
+  }
+  return !(n & (n - 1));
+}
