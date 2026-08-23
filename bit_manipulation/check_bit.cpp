1daa91cd5069c38f4807fcee33daef4546660bb8
pranshumg <pranshumg@gmail.com>
2026-08-23 17:00:41 +0530

    changed int64_t to uint64_t

--- bit_manipulation/check_bit.cpp
+++ bit_manipulation/check_bit.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 // left shift
 bool check_bit(int64_t n, int k) {
-  return n & (int64_t(1) << k);
+  return n & (uint64_t(1) << k);
 }
 
 // right shift
 bool check_bit(int64_t n, int k) {
-  return (n >> k) & int64_t(1);
+  return (n >> k) & uint64_t(1);
 }
--- bit_manipulation/set_bit.cpp
+++ bit_manipulation/set_bit.cpp
@@ -5,5 +5,5 @@ using namespace std;
 /* set the kth bit */
 
 void set_bit(int64_t& n, int k) {
-  n |= (int64_t(1) << k);
+  n |= (uint64_t(1) << k);
 }
