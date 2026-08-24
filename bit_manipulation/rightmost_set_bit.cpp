2f87814c8a6f6bdc2fe6813c17984c94d63646f5
pranshumg <pranshumg@gmail.com>
2026-08-24 19:26:49 +0530

    added more implementations

--- bit_manipulation/rightmost_set_bit.cpp
+++ bit_manipulation/rightmost_set_bit.cpp
@@ -6,5 +6,21 @@ using namespace std;
 
 // TC - O(1), SC - O(1)
 int rightmost_set_bit(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  return n ^ (n & (n - 1));
+}
+
+// TC - O(1), SC - O(1)
+int rightmost_set_bit(int n) {
   return n & -n;
+}
+
+// TC - O(1), SC - O(1)
+int rightmost_set_bit(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  return 1 << countr_zero(unsigned(n));
 }
