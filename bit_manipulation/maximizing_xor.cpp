7ef8f52d067d42868b25626ff49a1e0ffcba2f8b
pranshumg <pranshumg@gmail.com>
2026-08-26 22:56:35 +0530

    initial commit

---
+++ bit_manipulation/maximizing_xor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* maximizing xor */
+
+// TC - O(1), SC - O(1)
+int max_xor(int l, int r) {
+  if (l == r) {
+    return 0;
+  }
+  int pos = 32 - countl_zero(unsigned(l ^ r));
+  return (uint64_t(1) << pos) - 1;
+}
