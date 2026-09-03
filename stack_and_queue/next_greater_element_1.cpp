ffec356fc333ae40b7968b0437b583478d6d1bb3
pranshumg <pranshumg@gmail.com>
2026-09-03 14:14:31 +0530

    corrected

--- stack_and_queue/next_greater_element_1.cpp
+++ stack_and_queue/next_greater_element_1.cpp
@@ -6,15 +6,15 @@ using namespace std;
 // https://leetcode.com/problems/next-greater-element-i/
 
 // TC - O(n), SC - O(n)
-vector<int> next_greater_elements(vector<int>& v1, vector<int>& v2) { 
-  vector<int> nge(int(v2.size()));
+vector<int> next_greater_elements(vector<int>& v) { 
+  vector<int> nge(int(v.size()));
   stack<int> sk;
-  for (int i = int(v2.size()) - 1; i >= 0; i--) {
-    while (!sk.empty() && sk.top() <= v2[i]) {
+  for (int i = int(v.size()) - 1; i >= 0; i--) {
+    while (!sk.empty() && sk.top() <= v[i]) {
       sk.pop();
     }
     nge[i] = (!sk.empty() ? sk.top() : -1);
-    sk.push(v2[i]);
+    sk.push(v[i]);
   }
   return nge;
 }
